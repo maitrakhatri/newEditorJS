1a006cb7490d815c393426bd2cb71ee2fb6d570d
maitrakhatri <maitrakhatri@gmail.com>
2023-05-31 21:09:57 +0530

    feat: Fileverse dDocs

---
+++ src/vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+declare module '@editorjs/quote';
+declare module '@editorjs/warning';
+declare module '@editorjs/delimiter';
+declare module '@editorjs/list';
+declare module '@editorjs/checklist';
+declare module '@editorjs/simple-image';
+declare module '@editorjs/marker';
+declare module '@editorjs/code';
+declare module '@editorjs/link';
+declare module '@editorjs/embed';
+declare module '@editorjs/table';
+declare module '@editorjs/attaches';
+declare module '@editorjs/inline-code';
+declare module '@editorjs/underline';
+declare module '@editorjs/text-variant-tune';
+declare module 'editorjs-paragraph-with-alignment';
